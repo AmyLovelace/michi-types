256aaed64783dca0be30bc6bac730ffeae78366b
amanecer <amanecer.luz.cabrera@gmail.com>
2023-02-26 03:59:17 -0300

    added few lines

--- js/app.js
+++ js/app.js
@@ -17,6 +17,21 @@ var btnmenuopen=document.getElementById("btnmenuopen"),
 
 //cerrar menu con elemento de enlace
     enlaces.addEventListener("click",function () {
-        menuresponsive.style.transitionDelay
+        menuresponsive.style.transitionDelay="0.5s";
         menuresponsive.classList.remove("active");
-    })
+    });
+
+    //slider de productos
+    var contenedor=document.querySelector(".slider"),
+    btnizquierdo=document.getElementById("btn-izquierda"),
+    btnderecha=document.getElementById("btn-derecha");
+
+    //evento para el boton derecho
+    btnderecha.addEventListener("click",function() {
+      contenedor.scrollLeft += contenedor.offsetWidth;  
+        
+    });
+    btnizquierdo.addEventListener("click",function() {
+        contenedor.scrollLeft -= contenedor.offsetWidth;  
+          
+    });
